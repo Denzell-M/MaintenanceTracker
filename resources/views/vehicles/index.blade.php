8c316f669d4e81c0f39f3e2142063860477d9aad
Denzell <58393180+Denzell-M@users.noreply.github.com>
2026-05-21 14:26:31 -0400

    finished minimal table styles, added placeholder to record of vehicle

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -1,26 +1,28 @@
 <x-layout title="Index">
-    <div class="mx-auto max-w-6xl p-6">
+    <div class="mx-auto max-w-3xl p-6">
         <div class="overflow-hidden rounded-lg border border-grey-200 bg-white shadown-sm">
-            <table class="min-w-full divide-y divide-grey-200">
+            <table class="min-w-full table-fixed divide-y divide-grey-200">
                 <thead class="bg-grey-50">
-                    <tr>
-                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Make</th>
-                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Model</th>
-                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Odometer</th>
-                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Year</th>
+                    <tr class="divide-x divide-gray-200">
+                        <th class="w-1/4 px-6 py-3 text-left text-sm font-semibold text-gray-700">Make</th>
+                        <th class="w-1/4 px-6 py-3 text-left text-sm font-semibold text-gray-700">Model</th>
+                        <th class="w-1/4 px-6 py-3 text-left text-sm font-semibold text-gray-700">Odometer</th>
+                        <th class="w-1/4 px-6 py-3 text-left text-sm font-semibold text-gray-700">Year</th>
+                        <th class="w-1/4 px-6 py-3 text-left text-sm font-semibold text-gray-700"> Details </th>
                     </tr>
                 </thead>
 
                 <tbody class="divide-y divide-gray-200 bg-white">
                     @forelse ($vehicles as $vehicle)
-                    <tr>
-                        <td> {{ $vehicle->make }}</td>
-                        <td> {{ $vehicle->model }}</td>
-                        <td> {{ $vehicle->odometer }}</td>
-                        <td> {{ $vehicle->year }}</td>
+                    <tr class="divide-x divide-gray-200 hover:bg-gray-50">
+                        <td class="w-1/4 px-6 py-4 text-sm text-gray-700"> {{ $vehicle->make }}</td>
+                        <td class="w-1/4 px-6 py-4 text-sm text-gray-700"> {{ $vehicle->model }}</td>
+                        <td class="w-1/4 px-6 py-4 text-sm text-gray-700"> {{ $vehicle->odometer }}</td>
+                        <td class="w-1/4 px-6 py-4 text-sm text-gray-700"> {{ $vehicle->year }}</td>
+                        <td class="w-1/4 px-6 py-4 text-sm text-gray-700"> View </td>
                     @empty
                     <tr>
-                        <td colspan="4">No vehicles found.</td>
+                        <td colspan="4" class="px-6 py-6 text-center text-sm text-gray-500">No vehicles found.</td>
                     </tr>
                     @endforelse
                 </tbody>
