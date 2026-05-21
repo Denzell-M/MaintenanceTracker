c5a81b853f2b2fd7bc5edd6a1077766b06aaf52c
Denzell <58393180+Denzell-M@users.noreply.github.com>
2026-05-21 13:53:11 -0400

    added style to index of vehicles table

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -1,7 +1,30 @@
 <x-layout title="Index">
-    <div>
-        @foreach ($vehicles as $vehicle)
-            <p>{{ $vehicle->make }}: {{ $vehicle->model }}</p>
-        @endforeach
+    <div class="mx-auto max-w-6xl p-6">
+        <div class="overflow-hidden rounded-lg border border-grey-200 bg-white shadown-sm">
+            <table class="min-w-full divide-y divide-grey-200">
+                <thead class="bg-grey-50">
+                    <tr>
+                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Make</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Model</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Odometer</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold text-gray-700">Year</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200 bg-white">
+                    @forelse ($vehicles as $vehicle)
+                    <tr>
+                        <td> {{ $vehicle->make }}</td>
+                        <td> {{ $vehicle->model }}</td>
+                        <td> {{ $vehicle->odometer }}</td>
+                        <td> {{ $vehicle->year }}</td>
+                    @empty
+                    <tr>
+                        <td colspan="4">No vehicles found.</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-layout>
