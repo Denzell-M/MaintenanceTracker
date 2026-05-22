25a8b5396f691e903d21d0f7bdded921fa63ecb3
Denzell <58393180+Denzell-M@users.noreply.github.com>
2026-05-22 08:58:15 -0400

    added name to routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
 
-Route::get("/", [VehicleController::class, "index"]);
-Route::get("/vehicle/{vehicle}", [VehicleController::class, "show"]);
+Route::get("/", ([VehicleController::class, "index"])->name("vehicles.index"));
+Route::get(
+    "/vehicle/{vehicle}",
+    ([VehicleController::class, "show"])->name("vehicles.show"),
+);
